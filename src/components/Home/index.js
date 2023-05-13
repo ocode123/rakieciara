cc486b1fa1c42a158f00a6c61272a6f6288c247c
Oliweirdo <oliweirdo@gmail.com>
2023-05-13 13:42:36 +0200

    add bubble animation under photos

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,8 @@ const Home = () => {
                 <h3>programuję kosmosy i robię rakiety.</h3>
             </div>
             <img className="main-photo" src={mainPhoto} alt="" ></img>
-                 <div className='about'>
+            <div className="section">
+            <div className="about">
                 <p><h3 className="title">Lorem ipsum dolor sit amet.</h3> Sit cupiditate voluptatem sit rerum illum aut enim voluptas in impedit dicta.
                     Ea praesentium repellat et fuga vero ut velit nulla aut voluptate voluptatem. </p>
                 <p>Eum itaque sint et beatae iure ut quod velit sit quia quam ut labore voluptas ut quia autem.
@@ -20,7 +21,10 @@ const Home = () => {
                     Qui impedit accusantium hic adipisci autem 33 unde expedita
                     quo repudiandae <br />beatae sed odit galisum aut cumque velit vel quia aspernatur! </p>
             </div> 
+            </div>
+        
          <div className="wrapper">
+         <div className="bubblePhoto">
                 <section className="mosaic">
                     <div className="mosaic-wrap">
                         <div className="item material-1">
@@ -46,6 +50,7 @@ const Home = () => {
                     </div>
 
                 </section>
+                </div>
             </div>
 
        
